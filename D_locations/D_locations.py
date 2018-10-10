a12c03dc091aaa9624086efda96ca2ab804dbbb5
JustinJoco <justin.joco@gmail.com>
2018-10-10 19:44:41 -0400

    Revisions

--- D_locations/D_locations.py
+++ D_locations/D_locations.py
@@ -2,12 +2,9 @@ import scipy.io as sio
 import tensorflow as tf
 from tensorflow import keras
 import numpy as np
-import matplotlib.pyplot as plt
 import math
-from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.pyplot as plt
-from sklearn.preprocessing import MultiLabelBinarizer
-from keras.metrics import categorical_accuracy
+
 
 
 #Read .mat data into memory
@@ -23,6 +20,12 @@ location_content = sio.loadmat('mat_files/randLocationU2048VIP.mat')
 impulse_responses = D_content['D']
 locations = location_content['location']
 
+test_D_content = sio.loadmat('mat_files/D_VLOS_scaled_mean_mean_alpha6_fft_abs_Frobenius_sphericalWave_B32_U20000.mat')
+test_location_content = sio.loadmat('mat_files/randLocationU20000VIP.mat')
+
+test_impulse_responses = test_D_content['D']
+test_locations = test_location_content['location']
+
 
 d1, n = impulse_responses.shape
 d2, n = locations.shape
@@ -31,58 +34,17 @@ d2, n = locations.shape
 
 
 #train_length = math.floor(.8*n)
-impulses = np.transpose(impulse_responses)
-labels = np.transpose(locations[:2,:])
+impulses = np.transpose(test_impulse_responses)
+labels = np.transpose(test_locations[:2,:])
 
-train_length = math.floor(.8*n)
-test_impulses = impulses[train_length:,:]
-test_labels = labels[train_length:,:]
-
-'''
-train_impulses = np.transpose(impulse_responses[:, :train_length])
-train_labels = labels[:train_length,:]
-
-test_impulses = np.transpose(impulse_responses[:,train_length:])
-test_labels = labels[train_length:,:]
-
-temp = test_labels.copy()
-
-mlb = MultiLabelBinarizer()
-labels = mlb.fit_transform(labels)
-
-num_vectors, num_coordinates = labels.shape
-train_labels=labels[:train_length, :]
-test_labels=labels[train_length:, :]
+test_impulses = np.transpose(impulse_responses)
+test_labels = np.transpose(locations[:2,:])
 
 
 
 
-fig = plt.figure()
-ax = fig.add_subplot(111)
-
-ax.scatter(train_labels[:,0], train_labels[:,1], c='b', marker='o')
-
-ax.set_xlabel('X Label')
-ax.set_ylabel('Y Label')
-
-
-fig1 = plt.figure()
-ax = fig1.add_subplot(111)
-
-ax.scatter(test_labels[:,0], test_labels[:,1], c='r', marker='o')
-
-ax.set_xlabel('X Label')
-ax.set_ylabel('Y Label')
-
-
-
-
-plt.show()
-
-'''
-
-
-
+#Two hidden layers
+#One output layer
 model = keras.Sequential([
     keras.layers.Dense(100, activation='relu'),
     keras.layers.Dropout(.25),
@@ -93,37 +55,32 @@ model = keras.Sequential([
 
 model.compile(optimizer='adam',
               loss='mse',
-              metrics=['mape'])
+              metrics=['mae'])
 
-hist = model.fit(impulses, labels, epochs=100, batch_size=25, validation_split=.2)
-
-
+hist = model.fit(impulses, labels, epochs=100, batch_size=100, validation_split=.2)
 
 predictions = model.predict(test_impulses)
 
 
-
-
-
-
+#Plots mean absolute distance between predictions and actual data
 plt.figure(1)
 
 plt.subplot(211)
-plt.plot(hist.history['mean_absolute_percentage_error'])
-plt.plot(hist.history['val_mean_absolute_percentage_error'])
-plt.title('Model Abs Percentage Error')
-plt.ylabel('Percent Error')
+plt.plot(hist.history['mean_absolute_error'])
+plt.semilogy(hist.history['val_mean_absolute_error'])
+plt.title('Model Abs Error')
+plt.ylabel('Mean Error')
 plt.xlabel('epoch')
-plt.legend(['train','test'], loc='upper left')
+plt.legend(['train','validation'], loc='upper left')
 
 plt.subplot(212)
-
 plt.plot(hist.history['loss'])
-plt.plot(hist.history['val_loss'])
+plt.semilogy(hist.history['val_loss'])
 plt.title('model loss')
 plt.ylabel('loss')
 plt.xlabel('epoch')
-plt.legend(['train','test'], loc='upper left')
+plt.legend(['train','validation'], loc='upper left')
+
 
 
 predictions = model.predict(test_impulses)
