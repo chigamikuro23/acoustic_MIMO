b7196c9880a10c0a113ffc2dd105acb8e6dc0179
JustinJoco <justin.joco@gmail.com>
2019-03-25 19:01:40 -0400

    reorganized files

--- impulses_sim/gen_reflections.m
+++
@@ -1,8 +0,0 @@
-function reflections = gen_reflections(prev_tx, curr_tx, rx, walls, reflect_ctr, reflect_max)
-%GEN_REFLECTIONS Summary of this function goes here
-%   Detailed explanation goes here
-
-
-
-end
-
